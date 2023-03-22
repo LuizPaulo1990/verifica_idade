a1fc4a00aeba391ea7b4c217e38375f63d5de067
Luiz Paulo Martins dos Santos <117921451+LuizPaulo1990@users.noreply.github.com>
2023-03-22 14:34:32 -0300

    refiz o java

--- modelo.js
+++ modelo.js
@@ -1,56 +1,96 @@
 function verificar(){
-    
+   
     var data = new Date();
-    
+   
     var ano = data.getFullYear();
-    
-    var fano = document.getElementById('txtano');
-    
-    var res = document.querySelector('div#resultado');
-    
-    if (fano.value.length == 0 || Number(fano.value) > ano){
-        alert(' [ERRO] Verifique os dados e tente novament');
-    } else {
-       var fsex = document.getElementsByName('radsex');
-       var idade = ano - Number(fano.value);
-       var gênero = '';
-       var img = document.createElement('img');
-       img.setAttribute('id', 'foto');
-       if (fsex[0].checked){
-        gênero = 'Homem';
-        if (idade >= 0 && idade < 10){
-            //criança
-            img.setAttribute('src', 'imagens/garotinho.jpg');
-        } else if (idade < 21){
-            //jovem
-            img.setAttribute('src', 'imagens/rapaz.jpg');
-        } else if (idade < 50){
-            //adulto
-            img.setAttribute('src', 'imagens/homem.jpg');
-        } else{
-            //senhor
-            img.setAttribute('src', 'imagens/senhor.jpg');
-        }
-       } else if (fsex[1].checked){
-        gênero = 'Mulher';
-        if (idade >= 0 && idade < 10){
-            //criança
-            img.setAttribute('src', 'imagens/garotinha.jpg');
-        } else if (idade < 21){
-            //jovem
-            img.setAttribute('src', 'imagens/mocinha.jpg');
-        } else if (idade < 50){
-            //adulto
-            img.setAttribute('src', 'imagens/mulher.jpg');
-        } else{
-            //senhor
-            img.setAttribute('src', 'imagens/senhora.jpg');
-        }
-       }
-       res.style.textAlign = 'center';
-       res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`;
-       res.appendChild(img);
-    }
 
+    var fano = document.querySelector('input#txtano');
+    
+    var resultado = document.querySelector('div#resultado');
+    
+    if(fano.value.length == 0 || Number(fano.value) > ano){
+        
+        alert('[ERRO] Verifique os dados e tente novamente!');
+
+    } else{
+        
+        var fsex = document.getElementsByName('radsex');
+        
+        var idade = ano - Number(fano.value);
+        
+        var gênero = '';
+
+        var img = document.createElement('img');
+
+        if (fsex[0].checked){
+
+            gênero = 'Homem'
+
+            if(idade >= 0 && idade <10){
+            
+                img.setAttribute('src', '../imagens/garotinho.jpg');
+                
+                resultado.innerHTML = `Detectamos um garoto de ${idade} anos.`;
+
+            }else if (idade < 18){
+                
+                img.setAttribute('src', '../imagens/rapaz.jpg');
+                
+                resultado.innerHTML = `Detectamos um rapaz de ${idade} anos.`;
+
+            }else if (idade < 50){
+                
+                img.setAttribute('src', '../imagens/homem.jpg');
+                
+                resultado.innerHTML = `Detectamos um homem de ${idade} anos.`;
+
+            }else{
+                
+                img.setAttribute('src', '../imagens/senhor.jpg');
+                
+                resultado.innerHTML = `Detectamos um senhor de ${idade} anos.`;
+
+            }
+
+        } else {
+
+            gênero = 'Mulher';
+
+            if(idade >= 0 && idade <10){
+                
+                img.setAttribute('src', '../imagens/garotinha.jpg');
+                
+                resultado.innerHTML = `Detectamos uma garota de ${idade} anos.`;
+                
+            }else if (idade < 18){
+                
+                img.setAttribute('src', '../imagens/mocinha.jpg');
+               
+                resultado.innerHTML = `Detectamos uma mocinha de ${idade} anos.`;
+
+            }else if (idade < 50){
+                
+                img.setAttribute('src', '../imagens/mulher.jpg');
+
+                resultado.innerHTML = `Detectamos uma mulher de ${idade} anos.`;
+
+            }else{
+                
+                img.setAttribute('src', '../imagens/senhora.jpg');
+
+                resultado.innerHTML = `Detectamos uma senhora de ${idade} anos.`;
+            }
+
+        }
+        
+        resultado.style.textAlign = 'center';
+        
+        resultado.appendChild(img);
+
+
+
+    }
+   
+    
 
 }
